e927d257457240e68357fe13d81a2fc6de3c2103
Ubuntu <ubuntu@ip-172-31-87-84.ec2.internal>
2019-06-27 00:40:04 +0000

    get role and wst

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+app.get('/test', function (req, res) {
+    res.send('Role id: ' + process.env.ROLEID + '<br />' + 'Wrapped secret token: ' + process.env.WST);
+})
 app.listen(process.env.PORT || 3000);
  
 module.exports = app;
